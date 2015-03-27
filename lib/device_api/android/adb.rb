087d25fd7a51cf03595e61176d07b8930ea34725
Jon Wilson <jon.wilson01@bbc.co.uk>
2015-03-27 15:32:59 +0000

    Added rule for Kindle

--- lib/device_api/android/adb.rb
+++ lib/device_api/android/adb.rb
@@ -115,6 +115,29 @@ module DeviceAPI
         lines.last
       end
 
+      def self.generate_keystore(options = {})
+        keystore   = options[:keystore]
+        alias_name  = options[:alias]     || 'HiveTesting'
+        dname       = options[:dname]     || 'CN=hive'
+        password    = options[:password]  || 'hivetesting'
+
+        File.rename File.expand_path(keystore), "#{File.expand_path(keystore)}.backup" if File.exists?(File.expand_path(keystore))
+        result = execute("keytool -genkey -noprompt -alias #{alias_name} -dname '#{dname}' -keystore #{keystore} -storepass #{password} -keypass #{password} -keyalg RSA -keysize 2048 -validity 10000")
+        raise ADBCommandError.new(result.stderr) if result.exit != 0
+        true
+      end
+
+      def self.sign_apk(options = {})
+        apk               = options[:apk]
+        alias_name        = options[:alias]             || 'HiveTesting'
+        keystore          = options[:keystore]
+        keystore_password = options[:keystore_password] || 'hivetesting'
+
+        result = execute("jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore #{File.expand_path(keystore)} -storepass #{keystore_password} #{apk} #{alias_name}")
+        raise ADBCommandError.new(result.stderr) if result.exit != 0
+        true
+      end
+
       def self.get_uptime(serial)
         result = execute("adb -s #{serial} shell cat /proc/uptime")
 
