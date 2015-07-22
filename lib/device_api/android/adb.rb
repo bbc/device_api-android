6c5821fc5cdff9a8a520bcde5a37990dc72d4e1b
asim <asimk21@gmail.com>
2015-07-22 12:41:34 +0100

    if logic corrected

--- lib/device_api/android/adb.rb
+++ lib/device_api/android/adb.rb
@@ -234,6 +234,7 @@ module DeviceAPI
         result = execute("adb -s #{serial} shell dumpsys wifi | grep mNetworkInfo")
         if result.exit != 0
           raise ADBCommandError.new(result.stderr) 
+        else
           result = {:status => result.stdout.match("state:(.*?),")[1].strip, :access_point => result.stdout.match("extra:(.*?),")[1].strip.gsub(/"/,'')}
         end
         result  
