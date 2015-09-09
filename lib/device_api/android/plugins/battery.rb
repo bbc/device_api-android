43ba88e5885ea6516aa6fa3df079a89806c82d9d
Jon Wilson <jon.wilson01@bbc.co.uk>
2015-09-09 10:16:57 +0100

    Changed to attr_reader Fixed array splitting in memory plugin

--- lib/device_api/android/plugins/battery.rb
+++ lib/device_api/android/plugins/battery.rb
@@ -2,18 +2,18 @@ module DeviceAPI
   module Android
     module Plugin
       class Battery
-        attr_accessor :current_temp, :max_temp, :max_current, :voltage, :level, :health, :status
+        attr_reader :current_temp, :max_temp, :max_current, :voltage, :level, :health, :status
 
         def initialize(options = {})
           serial = options[:serial]
           props = ADB.get_battery_info(serial)
-          self.current_temp   = props["temperature"]
-          self.max_temp       = props["mBatteryMaxTemp"]
-          self.max_current    = props["mBatteryMaxCurrent"]
-          self.voltage        = props["voltage"]
-          self.level          = props["level"]
-          self.health         = props["health"]
-          self.status         = props["status"]
+          @current_temp   = props["temperature"]
+          @max_temp       = props["mBatteryMaxTemp"]
+          @max_current    = props["mBatteryMaxCurrent"]
+          @voltage        = props["voltage"]
+          @level          = props["level"]
+          @health         = props["health"]
+          @status         = props["status"]
         end
       end
     end
