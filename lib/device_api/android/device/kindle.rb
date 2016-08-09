4a34e3ab71ef35e8bfd3a0e545afcccf5babd6b0
Jon Wilson <jon.wilson01@bbc.co.uk>
2016-08-09 09:15:28 +0100

    Removed extra orientation call

--- lib/device_api/android/device/kindle.rb
+++ lib/device_api/android/device/kindle.rb
@@ -7,8 +7,11 @@ module DeviceAPI
       # work due to Amazons implementation of the Keyguard.
       def unlock
         ADB.keyevent(serial, '26') unless screen_on?
-        ADB.swipe(serial, { x_from: 500, y_from: 575, x_to: 500, y_to: 250 } ) if orientation == :landscape
-        ADB.swipe(serial, { x_from: 300, y_from: 900, x_to: 300, y_to: 600 } ) if orientation == :portrait
+        if orientation == :landscape
+          ADB.swipe(serial, { x_from: 500, y_from: 575, x_to: 500, y_to: 250 } )
+        else
+          ADB.swipe(serial, { x_from: 300, y_from: 900, x_to: 300, y_to: 600 } )
+        end
       end
     end
   end
