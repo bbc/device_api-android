f4db8d2a106271946335f778f5c0686fdd7222a9
Asim Khan <Asimk21@users.noreply.github.com>
2016-11-10 11:53:47 +0000

    Tests on travis CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,10 @@
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
+if ENV['CI']
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+else
+  require 'simplecov'
+  SimpleCov.start
+end
 
 $LOAD_PATH.unshift('./lib/')
 
