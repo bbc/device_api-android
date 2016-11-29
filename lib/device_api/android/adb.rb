c62e05615a98c53937e37e5b258fbe4b83d60a56
asim <asimk21@gmail.com>
2016-11-29 10:08:06 +0000

    Screenshot for android 7.0

--- lib/device_api/android/adb.rb
+++ lib/device_api/android/adb.rb
@@ -236,10 +236,11 @@ module DeviceAPI
       def self.screencap( qualifier, args )
         
         filename = args[:filename] or raise "filename not provided (:filename => '/tmp/myfile.png')"
-        
+ 
         convert_carriage_returns = %q{perl -pe 's/\x0D\x0A/\x0A/g'}
         cmd = "screencap -p | #{convert_carriage_returns} > #{filename}"
         
+        cmd = "screencap -p" if getprop(qualifier)['ro.build.version.release'].to_i > 6
         shell(qualifier, cmd)
       end
       
