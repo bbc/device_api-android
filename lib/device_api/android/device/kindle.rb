74edeccb47807b888bce922fecd04f54d0935da3
asim <asimk21@gmail.com>
2017-01-16 17:15:24 +0000

    deal with version<5

--- lib/device_api/android/device/kindle.rb
+++ lib/device_api/android/device/kindle.rb
@@ -15,7 +15,11 @@ module DeviceAPI
         res = resolution
         x = res[1].to_i
         y = res[2].to_i
-        { x_from: x/2, y_from: y -100, x_to: x/2, y_to: y/6}
+        if version.split('.').first.to_i < 5
+          {x_from: x - 100, y_from: y/2, x_to: x/6, y_to: y/2}
+        else
+          { x_from: x/2, y_from: y -100, x_to: x/2, y_to: y/6}
+        end
       end
 
     end
